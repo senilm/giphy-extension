b98590d11fe00a823bc3a249e6c32ea761bb7cb1
senilm <senilmendapara2003@gmail.com>
2024-06-05 22:43:12 +0530

    Modified: Navbar ui

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,7 +12,6 @@ import { navLinks } from "@/lib/consts";
 
 const Navbar = () => {
   const router = useRouter();
-  console.log()
   const pathname = usePathname();
   const logOut = async () => {
     await signOut(auth);
@@ -21,7 +20,7 @@ const Navbar = () => {
     Cookies.remove("userId");
     router.replace("/");
   };
-
+  const isActive = (href) => pathname.includes(href);
   return (
     <nav className="py-5 px-8 flex justify-between items-center">
       <Link href={"/"}>
@@ -33,9 +32,9 @@ const Navbar = () => {
           className=""
         />
       </Link>
-      <div className="gap-5 flex items-center">
+      <div className="gap-5 flex items-center transition-all">
         {navLinks.map((link)=>{
-            return <Link href={link.value} key={link.label}>{link.label}</Link>
+            return <Link href={link.value} key={link.label} className={isActive(link.value) ? ' underline-offset-4 underline' : ''}>{link.label}</Link>
         })}
         <Button type={"button"} onClick={logOut} variant="outline">
           Logout
